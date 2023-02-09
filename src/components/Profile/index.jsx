863657f26bcbebc28d8fb2c964da2e830463d978
sinazarrin <goldmontain8@gmail.com>
2023-02-09 18:35:33 +0330

    add profile component

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='w-1/4 h-[700px] mt-4 bg-white rounded-xl mx-10'>
+        
+    </div>
+  )
+}
+
+export default Profile
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
+import Profile from '../../components/Profile'
 
 const Dashboard = () => {
   return (
     <>
         <Header/>
-        <div className='flex h-full'>
+        <div className='flex justify-between h-full'>
             <Sidebar/>
-            
+            <Profile/>
         </div>
     </>
   )

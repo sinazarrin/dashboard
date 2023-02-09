96bf8590b5653a9095f76c95466877cbe139fc59
sinazarrin <goldmontain8@gmail.com>
2023-02-09 22:05:00 +0330

    add aboutMe section

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,5 +1,6 @@
 import UserPeofile from './UserProfile'
 import TaglineProfile from './TaglineProfile'
+import AboutMeProfile from './AboutMeProfile'
 
 
 const Profile = () => {
@@ -7,6 +8,7 @@ const Profile = () => {
         <div className='w-[330px] mt-4 bg-white rounded-xl mx-5 p-2'>
             <UserPeofile/>
             <TaglineProfile/>
+            <AboutMeProfile/>
         </div>
     )
 }

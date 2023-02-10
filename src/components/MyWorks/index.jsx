890476bf2243005de36fb75c63240a131e5ad137
sinazarrin <goldmontain8@gmail.com>
2023-02-10 19:10:02 +0330

    Add data file to render posts

--- src/components/MyWorks/index.jsx
+++ src/components/MyWorks/index.jsx
@@ -1,53 +1,7 @@
-import { useState } from "react"
 import AllWorks from "./AllWork"
+import { works } from "../../data/data"
 
 const MyWorks = () => {
-
-  const [works, setWorks] = useState([
-
-    {
-      id: 2,
-      title: 'گویندگی و خوانندگی',
-      posts: [
-        {
-          id: 1,
-          image: './assets/images/announcer1.jpg',
-          caption: 'دوبله و تقلید صدا'
-        },
-        {
-          id: 2,
-          image: './assets/images/announcer2.jpg',
-          caption: 'گویندگی و اخبار گویی'
-        },
-        {
-          id: 3,
-          image: './assets/images/announcer3.jpg',
-          caption: 'خوانش کتاب صوتی "کتابخانه نیمه شب"'
-        }
-      ]
-    },
-    {
-      id: 3,
-      title: 'تدوین و صداگذاری',
-      posts: [
-        {
-          id: 1,
-          image: './assets/images/Editing-and-voiceover1.jpg',
-          caption: 'تبلیغ تیزر تبلیغاتی نوشیدنی انرژی زا'
-        },
-        {
-          id: 2,
-          image: './assets/images/Editing-and-voiceover2.jpg',
-          caption: 'تبلیغ تیزر تبلیغاتی نوشیدنی انرژی زا'
-        },
-        {
-          id: 3,
-          image: './assets/images/Editing-and-voiceover3.jpg',
-          caption: 'خوانندگی در تیتراژ سریال'
-        }
-      ]
-    },
-  ])
 
   return (
     <div className='bg-[#F3F6FF] dark:bg-gray-900 p-5 overflow-x-scroll w-full'>
---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+export const works = [
+    [
+
+        {
+            id: 2,
+            title: 'گویندگی و خوانندگی',
+            posts: [{
+                    id: 1,
+                    image: './assets/images/announcer1.jpg',
+                    caption: 'دوبله و تقلید صدا'
+                },
+                {
+                    id: 2,
+                    image: './assets/images/announcer2.jpg',
+                    caption: 'گویندگی و اخبار گویی'
+                },
+                {
+                    id: 3,
+                    image: './assets/images/announcer3.jpg',
+                    caption: 'خوانش کتاب صوتی "کتابخانه نیمه شب"'
+                }
+            ]
+        },
+        {
+            id: 3,
+            title: 'تدوین و صداگذاری',
+            posts: [{
+                    id: 1,
+                    image: './assets/images/Editing-and-voiceover1.jpg',
+                    caption: 'تبلیغ تیزر تبلیغاتی نوشیدنی انرژی زا'
+                },
+                {
+                    id: 2,
+                    image: './assets/images/Editing-and-voiceover2.jpg',
+                    caption: 'تبلیغ تیزر تبلیغاتی نوشیدنی انرژی زا'
+                },
+                {
+                    id: 3,
+                    image: './assets/images/Editing-and-voiceover3.jpg',
+                    caption: 'خوانندگی در تیتراژ سریال'
+                }
+            ]
+        },
+    ]
+]
